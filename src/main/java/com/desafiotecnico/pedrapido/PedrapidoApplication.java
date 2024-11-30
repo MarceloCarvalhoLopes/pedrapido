0ea4d6cd226bed34bc3a39ad50d2bcd85dc0faf5
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-11-30 11:51:32 -0300

    first commit

---
+++ src/main/java/com/desafiotecnico/pedrapido/PedrapidoApplication.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.pedrapido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedrapidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedrapidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiotecnico/pedrapido/PedrapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiotecnico.pedrapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedrapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
